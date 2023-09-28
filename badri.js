c01ceb1be094c2f0f310b54f969c5198630f1220
BEZAWADA BADRINATH <21pa1a0418@vishnu.edu.in>
2023-09-28 14:48:45 +0530

    second commit

--- badri.js
+++ badri.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const passwordHash = require('password-hash');
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const axios = require('axios');
@@ -31,7 +32,7 @@ app.post("/login",async (req, res) => {
     
     try {
         const usersRef = db.collection("personal info");
-        const snapshot = await usersRef.where("Email", "==", email).where("Password", "==", password).get();
+        const snapshot = await usersRef.where("Email", "==", email).get();
         
         if (snapshot.empty) {
             // No matching user found
@@ -41,12 +42,24 @@ app.post("/login",async (req, res) => {
             };
             res.json(failureResponse);
         } else {
+            const userDoc = snapshot.docs[0].data();
+            const storedHashedPassword = userDoc.Password;
+
+            if (passwordHash.verify(password, storedHashedPassword)) {
             // Matching user found, login successful
             const successResponse = {
                 success: true,
                 message: "Login successful"
             };
             res.json(successResponse);
+        } else {
+            // Passwords do not match, login failed
+            const failureResponse = {
+                success: false,
+                message: "Login failed"
+            };
+            res.json(failureResponse);
+        }
         }
     } catch (error) {
         console.error("Error during login:", error);
@@ -57,35 +70,49 @@ app.post("/login",async (req, res) => {
     }
 });
 
-app.post("/signupsubmit", (req, res) => {
+app.post("/signupsubmit", async (req, res) => {
     // Handle signup submit logic here
     const fullName = req.body.FullName;
     const email = req.body.Email;
     const password = req.body.Password;
 
-    // Store user information in Firestore
-    db.collection("personal info")
-        .add({
-            FullName: fullName,
-            Email: email,
-            Password: password,
-        })
-        .then(() => {
+    try {
+        const usersRef = db.collection("personal info");
+
+        // Check if the email already exists
+        const existingUser = await usersRef.where("Email", "==", email).get();
+
+        if (!existingUser.empty) {
+            // Email already exists, return an error response
+            const failureResponse = {
+                success: false,
+                message: "Email already exists"
+            };
+            res.json(failureResponse);
+        } else {
+            // Email is unique, proceed with signup
+            const hashedPassword = passwordHash.generate(password);
+            await usersRef.add({
+                FullName: fullName,
+                Email: email,
+                Password: hashedPassword,
+            });
+
             const successResponse = {
                 success: true,
                 message: "Signup successful"
             };
             res.json(successResponse);
-            
-        })
-        .catch(error => {
-            console.error("Error adding document: ", error);
-            res.status(500).json({
-                success: false,
-                message: "An error occurred while processing your request."
-            });
+        }
+    } catch (error) {
+        console.error("Error during signup:", error);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while processing your request."
         });
+    }
 });
+
 // Handle news API request
 app.get("/getNews", async (req, res) => {
     try {
